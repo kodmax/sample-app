1f747fcb23fb51c91da3b3d126f90c5dabd02de4
mm.kodmax@gmail.com <mm.kodmax@gmail.com>
2016-01-06 18:09:55 +0100

    rest

--- webapp/app/scripts/controllers/grid/grid-controller.js
+++ webapp/app/scripts/controllers/grid/grid-controller.js
@@ -4,13 +4,13 @@ define([], function () {
 	var GridController = ['$scope', function ($scope) {
 		
 		$scope.rows = [
-		    { mode: 'view', data: { id: 1, name: 'name1', surname: 'surname1', birthdate: '24-8-1981', mobile:'1231241234', address: 'Poznań, Długa 2' } },
-		    { mode: 'view', data: { id: 2, name: 'name2', surname: 'surname2', birthdate: '24-10-1981', mobile:'3223423', address: 'Poznań, Długa 3' } },
-		    { mode: 'view', data: { id: 3, name: 'name3', surname: 'surname3', birthdate: '24-9-1981', mobile:'1234432', address: 'Poznań, Długa 4' } },
-		    { mode: 'view', data: { id: 4, name: 'name4', surname: 'surname4', birthdate: '24-1-1981', mobile:'7686785', address: 'Poznań, Długa 5' } },
-		    { mode: 'view', data: { id: 5, name: 'name5', surname: 'surname5', birthdate: '24-2-1981', mobile:'4563534', address: 'Poznań, Długa 6' } },
-		    { mode: 'view', data: { id: 6, name: 'name6', surname: 'surname6', birthdate: '24-3-1981', mobile:'34657645', address: 'Poznań, Długa 7' } },
-		    { mode: 'view', data: { id: 7, name: 'name7', surname: 'surname7', birthdate: '24-5-1981', mobile:'234235345', address: 'Poznań, Długa 8' } }
+		    { mode: 'view', state: 'idle', data: { id: 1, name: 'name1', surname: 'surname1', birthdate: '24-8-1981', mobile:'1231241234', address: 'Poznań, Długa 2' } },
+		    { mode: 'view', state: 'idle', data: { id: 2, name: 'name2', surname: 'surname2', birthdate: '24-10-1981', mobile:'3223423', address: 'Poznań, Długa 3' } },
+		    { mode: 'view', state: 'idle', data: { id: 3, name: 'name3', surname: 'surname3', birthdate: '24-9-1981', mobile:'1234432', address: 'Poznań, Długa 4' } },
+		    { mode: 'view', state: 'idle', data: { id: 4, name: 'name4', surname: 'surname4', birthdate: '24-1-1981', mobile:'7686785', address: 'Poznań, Długa 5' } },
+		    { mode: 'view', state: 'idle', data: { id: 5, name: 'name5', surname: 'surname5', birthdate: '24-2-1981', mobile:'4563534', address: 'Poznań, Długa 6' } },
+		    { mode: 'view', state: 'idle', data: { id: 6, name: 'name6', surname: 'surname6', birthdate: '24-3-1981', mobile:'34657645', address: 'Poznań, Długa 7' } },
+		    { mode: 'view', state: 'idle', data: { id: 7, name: 'name7', surname: 'surname7', birthdate: '24-5-1981', mobile:'234235345', address: 'Poznań, Długa 8' } }
 	    ];
 		
 		$scope.edit = function (row) {
@@ -26,7 +26,7 @@ define([], function () {
 			row.state = 'saving';
 			
 			$.ajax({
-				url: 'api/user/' + row.data.id,
+				url: 'http://users/impaqgroup.com/edit/user.' + row.data.id,
 				method: 'post',
 				contentType: 'application/json; charset=UTF-8',
 				data: JSON.stringify(row.edit),
@@ -34,14 +34,37 @@ define([], function () {
 				success: function () {
 					$scope.$apply(function () {
 						row.mode = 'view';
-						row.state = 'ok';
+						row.state = 'idle';
 					});					
 				},
 				
 				error: function () {
 					$scope.$apply(function () {
 						row.mode = 'view';
-						row.state = 'error';
+						row.state = 'idle';
+					});					
+				}
+			});
+		};
+		
+		$scope.remove = function (row) {
+			row.state = 'deleting';
+			
+			$.ajax({
+				url: 'http://users/impaqgroup.com/remove/user.' + row.data.id,
+				method: 'post',
+				contentType: 'application/json; charset=UTF-8',
+				data: JSON.stringify(row.edit),
+				
+				success: function () {
+					$scope.$apply(function () {
+						$scope.rows.splice($scope.rows.indexOf(row), 1);
+					});					
+				},
+				
+				error: function () {
+					$scope.$apply(function () {
+						$scope.rows.splice($scope.rows.indexOf(row), 1);
 					});					
 				}
 			});
